3a90d97293b65e553aa82218aae993b054afaad8
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-06-15 12:37:57 -0400

    Fix codegen example interfaces
    create() and collect() APIs have changed

--- examples/codegen.py
+++ examples/codegen.py
@@ -11,13 +11,13 @@ def protocol():
         ("INTEGER", set([1, 2, 3])),
         ("INTEGER", set([1, 2, 3]))
     ]
-    inA = sal.create("inA", colsInA)
+    inA = sal.create("inA", colsInA, set([1]))
 
     # specify the workflow
     agg = sal.aggregate(inA, "agg", "inA_0", "inA_1", "+")
     projA = sal.project(agg, "projA", ["agg_0", "agg_1"])
     projB = sal.project(projA, "projB", ["projA_0", "projA_1"])
-    opened = sal.collect(projB, "opened", 1)
+    opened = sal.collect(projB, 1)
 
     # return root nodes
     return set([inA])
