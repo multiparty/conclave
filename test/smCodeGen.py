ee784ab4f411eec595fbe7fc6c937396bee53118
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2017-07-10 17:15:23 +0200

    Sharemind agg codegen fake test case.

--- test/smCodeGen.py
+++ test/smCodeGen.py
@@ -25,6 +25,31 @@ def testStore():
     print(actual["input"])
     print(actual["protocol"])
 
+def testAgg():
+
+    @sharemind
+    @dagonly
+    def protocol():
+
+        # define inputs
+        colsIn1 = [
+            defCol("INTEGER", [1]),
+            defCol("INTEGER", [1])
+        ]
+        in1 = sal.create("in1", colsIn1, set([1]))
+        out = sal._store(in1, "out", set([1, 2, 3]))
+        agg = sal.aggregate(out, "agg", "out_0", "out_1", "+")
+
+        # return root nodes
+        return set([in1])
+
+    actual = protocol()
+    print(actual["schemas"])
+    print(actual["input"])
+    print(actual["protocol"])
+
 if __name__ == "__main__":
 
     testStore()
+    testAgg()
+    
