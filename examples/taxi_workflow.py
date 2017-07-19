3f6931e8dcc9a6c5a9ce03cba92119137b65e82b
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-07-19 18:50:20 -0400

    Human-readable relation names in Taxi workflow
    We should use named columns as well, but that requires some other
    changes.

--- examples/taxi_workflow.py
+++ examples/taxi_workflow.py
@@ -27,23 +27,31 @@ def protocol():
         defCol("STRING", [1, 2, 3]),
         defCol("INTEGER", [1, 2, 3])
     ]
-    inA = sal.create("inA", colsInA, set([1]))
+    cab_data = sal.create("cab_data", colsInA, set([1]))
 
-    projA = sal.project(inA, "projA", ["inA_0", "inA_17"])
-    aggA = sal.aggregate(projA, "aggA", "projA_0", "projA_1", "+")
-    divA = sal.divide(aggA, "divA", "aggA_1", ["aggA_1", 1000])
-    multA = sal.multiply(divA, "multA", "divA_0", ["divA_0", 0])
-    multB = sal.multiply(multA, "multB", "multA_1", ["multA_1", 100])
-    aggB = sal.aggregate(multA, "aggB", "multA_0", "multA_1", "+")
-    joinA = sal.join(multB, aggB, "joinA", "multB_0", "aggB_0")
-    divB = sal.divide(joinA, "divB", "joinA_1", ["joinA_1", "joinA_2"])
-    multC = sal.multiply(divB, "multC", "divB_1", ["divB_1", "divB_1"])
-    aggC = sal.aggregate(multC, "aggC", "multC_1", "multC_0", "+")
+    selected_input = sal.project(cab_data, "selected_input", ["cab_data_0", "cab_data_17"])
+    local_rev = sal.aggregate(selected_input, "local_rev", "selected_input_0",
+                              "selected_input_1", "+")
+    scaled_down = sal.divide(local_rev, "scaled_down", "local_rev_1", ["local_rev_1", 1000])
+    first_val_blank = sal.multiply(scaled_down, "first_val_blank", "scaled_down_0",
+                                   ["scaled_down_0", 0])
+    local_rev_scaled = sal.multiply(first_val_blank, "local_rev_scaled", "first_val_blank_1",
+                                    ["first_val_blank_1", 100])
+    total_rev = sal.aggregate(first_val_blank, "total_rev", "first_val_blank_0",
+                              "first_val_blank_1", "+")
+    local_total_rev = sal.join(local_rev_scaled, total_rev, "local_total_rev",
+                               "local_rev_scaled_0", "total_rev_0")
+    market_share = sal.divide(local_total_rev, "market_share", "local_total_rev_1",
+                              ["local_total_rev_1", "local_total_rev_2"])
+    market_share_squared = sal.multiply(market_share, "market_share_squared", "market_share_1",
+                                        ["market_share_1", "market_share_1"])
+    hhi = sal.aggregate(market_share_squared, "hhi", "market_share_squared_1",
+                        "market_share_squared_0", "+")
 
-    opened = sal.collect(aggC, 1)
+    opened = sal.collect(hhi, 1)
 
     # return root nodes
-    return set([inA])
+    return set([cab_data])
 
 if __name__ == "__main__":
 
