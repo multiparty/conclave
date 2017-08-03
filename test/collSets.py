bfec3b3f54f74dfa2ed3d217f850de57ff94c3ad
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-08-02 23:35:26 -0400

    Whitespace fixes

--- test/collSets.py
+++ test/collSets.py
@@ -11,7 +11,7 @@ def testMergeCollSets():
 
     left = set([frozenset([1, 2]), frozenset([3, 4])])
     right = set([frozenset([5, 6]), frozenset([7])])
-    
+
     actual = mergeCollSets(left, right)
     expected = {frozenset({1, 2, 5, 6}), frozenset({1, 2, 7}), frozenset({3, 4, 5, 6}), frozenset({3, 4, 7})}
 
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     testMergeCollSets()
     testDefCol()
     print("All OK")
-    
+
