d116d3ce98c54cb71ee9b4b2baee0158ebefc6ba
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-08-04 00:16:04 -0400

    Named column outputs for all pruneDag tests
    These all pass now; rewriteDag and smCodeGen still to be converted.

--- test/pruneDag.py
+++ test/pruneDag.py
@@ -169,17 +169,17 @@ def testAggProjProjOther():
         # return root nodes
         return set([in1, in2])
 
-    expected = """CLOSE agg_1([agg_1_0 {2}, agg_1_1 {2}]) {2} INTO agg_1_close([agg_1_close_0 {2}, agg_1_close_1 {2}]) {1, 2}
-CREATE RELATION in1([in1_0 {1}, in1_1 {1}]) {1} WITH COLUMNS (INTEGER, INTEGER)
-PROJECT [in1_0, in1_1] FROM (in1([in1_0 {1}, in1_1 {1}]) {1}) AS projA_0([projA_0_0 {1}, projA_0_1 {1}]) {1}
-PROJECT [projA_0_0, projA_0_1] FROM (projA_0([projA_0_0 {1}, projA_0_1 {1}]) {1}) AS projB_0([projB_0_0 {1}, projB_0_1 {1}]) {1}
-AGG [projB_0_1, +] FROM (projB_0([projB_0_0 {1}, projB_0_1 {1}]) {1}) GROUP BY [projB_0_0] AS agg_0([agg_0_0 {1}, agg_0_1 {1}]) {1}
-CLOSE agg_0([agg_0_0 {1}, agg_0_1 {1}]) {1} INTO agg_0_close([agg_0_close_0 {1}, agg_0_close_1 {1}]) {1, 2}
-CONCATMPC [agg_0_close([agg_0_close_0 {1}, agg_0_close_1 {1}]) {1, 2}, agg_1_close([agg_1_close_0 {2}, agg_1_close_1 {2}]) {1, 2}] AS rel([rel_0 {1,2}, rel_1 {1,2}]) {1, 2}
-AGGMPC [rel_1, +] FROM (rel([rel_0 {1,2}, rel_1 {1,2}]) {1, 2}) GROUP BY [rel_0] AS agg_obl([agg_obl_0 {1,2}, agg_obl_1 {1,2}]) {1, 2}
-OPEN agg_obl([agg_obl_0 {1,2}, agg_obl_1 {1,2}]) {1, 2} INTO agg_obl_open([agg_obl_open_0 {1,2}, agg_obl_open_1 {1,2}]) {1}
-PROJECT [agg_obl_open_0, agg_obl_open_1] FROM (agg_obl_open([agg_obl_open_0 {1,2}, agg_obl_open_1 {1,2}]) {1}) AS projC([projC_0 {1,2}, projC_1 {1,2}]) {1}
-PROJECT [projC_0, projC_1] FROM (projC([projC_0 {1,2}, projC_1 {1,2}]) {1}) AS projD([projD_0 {1,2}, projD_1 {1,2}]) {1}
+    expected = """CLOSE agg_1([a {2}, total_b {2}]) {2} INTO agg_1_close([a {2}, total_b {2}]) {1, 2}
+CREATE RELATION in1([a {1}, b {1}]) {1} WITH COLUMNS (INTEGER, INTEGER)
+PROJECT [a, b] FROM (in1([a {1}, b {1}]) {1}) AS projA_0([a {1}, b {1}]) {1}
+PROJECT [a, b] FROM (projA_0([a {1}, b {1}]) {1}) AS projB_0([a {1}, b {1}]) {1}
+AGG [b, +] FROM (projB_0([a {1}, b {1}]) {1}) GROUP BY [a] AS agg_0([a {1}, total_b {1}]) {1}
+CLOSE agg_0([a {1}, total_b {1}]) {1} INTO agg_0_close([a {1}, total_b {1}]) {1, 2}
+CONCATMPC [agg_0_close([a {1}, total_b {1}]) {1, 2}, agg_1_close([a {2}, total_b {2}]) {1, 2}] AS rel([a {1,2}, b {1,2}]) {1, 2}
+AGGMPC [b, +] FROM (rel([a {1,2}, b {1,2}]) {1, 2}) GROUP BY [a] AS agg_obl([a {1,2}, total_b {1,2}]) {1, 2}
+OPEN agg_obl([a {1,2}, total_b {1,2}]) {1, 2} INTO agg_obl_open([a {1,2}, total_b {1,2}]) {1}
+PROJECT [a, total_b] FROM (agg_obl_open([a {1,2}, total_b {1,2}]) {1}) AS projC([a {1,2}, total_b {1,2}]) {1}
+PROJECT [a, total_b] FROM (projC([a {1,2}, total_b {1,2}]) {1}) AS projD([a {1,2}, total_b {1,2}]) {1}
 """
     actual = protocol()
     assert expected == actual, actual
@@ -206,12 +206,12 @@ def testJoin():
         sal.collect(mult, 1)
         return set([inA, inB])
 
-    expected = """CREATE RELATION inA([inA_0 {1}, inA_1 {1}]) {1} WITH COLUMNS (INTEGER, INTEGER)
-CLOSE inA([inA_0 {1}, inA_1 {1}]) {1} INTO inA_close([inA_close_0 {1}, inA_close_1 {1}]) {1, 2}
-CLOSE projB([projB_0 {2}, projB_1 {2}]) {2} INTO projB_close([projB_close_0 {2}, projB_close_1 {2}]) {1, 2}
-(inA_close([inA_close_0 {1}, inA_close_1 {1}]) {1, 2}) JOINMPC (projB_close([projB_close_0 {2}, projB_close_1 {2}]) {1, 2}) ON inA_close_0 AND projB_close_0 AS joined([joined_0 {1,2}, joined_1 {1,2}, joined_2 {1,2}]) {1, 2}
-MULTIPLYMPC [joined_0 -> joined_0 * 0] FROM (joined([joined_0 {1,2}, joined_1 {1,2}, joined_2 {1,2}]) {1, 2}) AS mult([mult_0 {1,2}, mult_1 {1,2}, mult_2 {1,2}]) {1, 2}
-OPEN mult([mult_0 {1,2}, mult_1 {1,2}, mult_2 {1,2}]) {1, 2} INTO mult_open([mult_open_0 {1,2}, mult_open_1 {1,2}, mult_open_2 {1,2}]) {1}
+    expected = """CREATE RELATION inA([a {1}, b {1}]) {1} WITH COLUMNS (INTEGER, INTEGER)
+CLOSE inA([a {1}, b {1}]) {1} INTO inA_close([a {1}, b {1}]) {1, 2}
+CLOSE projB([c {2}, d {2}]) {2} INTO projB_close([c {2}, d {2}]) {1, 2}
+(inA_close([a {1}, b {1}]) {1, 2}) JOINMPC (projB_close([c {2}, d {2}]) {1, 2}) ON a AND c AS joined([a {1,2}, b {1,2}, d {1,2}]) {1, 2}
+MULTIPLYMPC [a -> a * 0] FROM (joined([a {1,2}, b {1,2}, d {1,2}]) {1, 2}) AS mult([a {1,2}, b {1,2}, d {1,2}]) {1, 2}
+OPEN mult([a {1,2}, b {1,2}, d {1,2}]) {1, 2} INTO mult_open([a {1,2}, b {1,2}, d {1,2}]) {1}
 """
     actual = protocol()
     assert expected == actual, actual
