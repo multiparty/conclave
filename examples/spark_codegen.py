41529b9127b014f2707ce78f41695afda1c2b233
Ben Getchell <bengetch@gmail.com>
2017-08-17 14:37:58 -0400

    added test for spark codegen of multicol_join

--- examples/spark_codegen.py
+++ examples/spark_codegen.py
@@ -123,37 +123,65 @@ def multicol_agg():
 
     return set([in1])
 
+
+@dagonly
+def multicol_join():
+    colsInA = [
+        defCol("a", "INTEGER", [1, 2, 3]),
+        defCol("b", "INTEGER", [1, 2, 3]),
+        defCol("c", "INTEGER", [1, 2, 3])
+    ]
+
+    colsInB = [
+        defCol("a", "INTEGER", [1, 2, 3]),
+        defCol("b", "INTEGER", [1, 2, 3]),
+        defCol("d", "INTEGER", [1, 2, 3])
+    ]
+
+    in1 = sal.create('in1', colsInA, set([1, 2, 3]))
+    in2 = sal.create('in2', colsInB, set([1, 2, 3]))
+
+    join1 = sal.join(in1, in2, 'join1', ['a', 'b'], ['a', 'b'])
+
+    opened = sal.collect(join1, 1)
+
+    return set([in1])
+
 if __name__ == "__main__":
 
     '''
     simple_mult_dag = simple_mult()
-    comp_mult_dag = comp_mult()
-    simple_div_dag = simple_div()
-    comp_div_dag = comp_div()
-    create_mult_dag = create_col_mult()
-    create_div_dag = create_col_div()
-
     simple_mult = spark.SparkCodeGen(simple_mult_dag)
     simple_mult.generate("simple_mult", "/tmp")
 
+    comp_mult_dag = comp_mult()
     comp_mult = spark.SparkCodeGen(comp_mult_dag)
     comp_mult.generate("comp_mult", "/tmp")
 
+    simple_div_dag = simple_div()
     simple_div = spark.SparkCodeGen(simple_div_dag)
     simple_div.generate("simple_div", "/tmp")
 
+    comp_div_dag = comp_div()
     comp_div = spark.SparkCodeGen(comp_div_dag)
     comp_div.generate("comp_div", "/tmp")
 
+    create_mult_dag = create_col_mult()
     create_mult = spark.SparkCodeGen(create_mult_dag)
     create_mult.generate("create_mult", "/tmp")
 
+    create_div_dag = create_col_div()
     create_div = spark.SparkCodeGen(create_div_dag)
     create_div.generate("create_div", "/tmp")
-    '''
 
     multicol_agg_dag = multicol_agg()
     multicol_agg = spark.SparkCodeGen(multicol_agg_dag)
     multicol_agg.generate('multicol_agg', '/tmp')
+    '''
+
+    multicol_join_dag = multicol_join()
+    multicol_join = spark.SparkCodeGen(multicol_join_dag)
+    multicol_join.generate('multicol_join', '/tmp')
+
 
     print("Spark code generated in /tmp/")
