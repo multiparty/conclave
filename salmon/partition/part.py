4b380fb9a77c3055a445f718476407d7ec285daf
Ben Getchell <bengetch@gmail.com>
2017-08-22 14:39:41 -0400

    Implemented partitioning algorithm. Currently writing tests to confirm correctness. Still need to implement the measureCost() function which measures the cost of running a node for a given backend, but will need to wait til Thursday to chat about how best to do that.

--- salmon/partition/part.py
+++ salmon/partition/part.py
@@ -1,6 +1,7 @@
 from salmon.dag import Dag
-from math import inf
+from math import inf, pow
 from salmon.codegen import spark, sharemind
+from sys import maxsize
 
 
 class SubDag(Dag):
@@ -29,8 +30,8 @@ class SubDag(Dag):
 
     def findRoots(self):
 
-        def isLocalRoot(node, all_nodes):
-            return len(set.intersection(node.parents, set(all_nodes))) == 0
+        def isLocalRoot(current_node, all_nodes):
+            return len(set.intersection(current_node.parents, set(all_nodes))) == 0
 
         roots = []
 
@@ -46,29 +47,89 @@ class SubDag(Dag):
         return inf
 
 
-# assuming that each partition will be a list of lists
-def getAllPartitions(dag):
-    return []
+def partitionDag(dag):
+    sorted_nodes = dag.topSort()
+    best = getBestPartition(sorted_nodes)
+
+    return best
 
 
-def measureCost(partition):
-    cost = 0
-    for job in partition:
-        cost += job.getCost(job)
-    return cost
+def getBestPartition(nodes):
+    max_cost = maxsize
+    num_ops = len(nodes)
+    max_ops = pow(2, num_ops)
+    cost = [[False for i in range(max_ops)] for j in range(max_cost)]
+    cost[0][0] = True
+    parent = [[0 for i in range(max_ops)] for j in range(max_cost)]
+    parent_cost = [[0 for i in range(max_ops)] for j in range(max_cost)]
+    scheduled_fmwk = [["spark" for i in range(max_ops)] for j in range(max_cost)]
+
+    min_cost = [inf for i in range(max_ops)]
+    job_cost = [inf for i in range(max_ops)]
+    min_fmwk = ['' for i in range(max_ops)]
+    min_cost[0] = 0
+    job_cost[0] = 0
+
+    all_ops_flag = max_ops - 1
+
+    # TODO: this is a hacky way to iterate over frameworks
+    fmwks = ['spark', 'sharemind', 'viff']
+    for i in range(1, max_ops):
+        merge_nodes = []
+        for j in range(num_ops):
+            if (1 << j) & i:
+                merge_nodes.append(nodes[j])
+        # iterate over frameworks to find one of least cost,
+        # w/r/t a selection of merged nodes
+        for fmwk in fmwks:
+            this_cost = measureCost(merge_nodes, fmwk)
+            if this_cost < min_cost[i] and this_cost < max_cost:
+                min_cost[i] = this_cost
+                job_cost[i] = this_cost
+                min_fmwk[i] = fmwk
+    for cur_cost in range(max_cost):
+        # flag to indicate when best solution is reached
+        if cost[cur_cost][all_ops_flag]:
+            break
+        for jobs_exec in range(all_ops_flag):
+            if cost[cur_cost][jobs_exec] and cur_cost <= min_cost[jobs_exec]:
+                for jobs_to_merge in range(jobs_exec + 1, all_ops_flag):
+                    jobs_merged = (jobs_to_merge & jobs_exec) ^ jobs_to_merge
+                    next_jobs_exec = jobs_merged | jobs_exec
+                    next_cost = cur_cost + job_cost[jobs_merged]
+                    if (cost[next_cost][next_jobs_exec] is False) and next_cost < max_cost:
+                        cost[next_cost][next_jobs_exec] = True
+                        parent[next_cost][next_jobs_exec] = jobs_exec
+                        parent_cost[next_cost][next_jobs_exec] = cur_cost
+                        if next_cost < min_cost[next_jobs_exec]:
+                            min_cost[next_jobs_exec] = next_cost
+                            min_fmwk[next_jobs_exec] = min_fmwk[jobs_merged]
+                        scheduled_fmwk[next_cost][next_jobs_exec] = min_fmwk[jobs_merged]
+    result = []
+
+    cur_jobs_exec = all_ops_flag
+    while cur_jobs_exec > 0:
+        subdag = []
+        assert cur_cost >= max_cost, \
+            "At least one operator could not be mapped to a backend."
+        # assert parent[final_cost] is not None
+        prev_jobs_exec = parent[cur_cost][cur_jobs_exec]
+        jobs_merged = prev_jobs_exec ^ cur_jobs_exec
+        for num_op in range(num_ops):
+            if pow(2, num_op) & jobs_merged:
+                subdag.append(nodes[num_op])
+        result.append((SubDag(subdag), scheduled_fmwk[cur_cost][cur_jobs_exec]))
+        tmp_jobs_exec = cur_jobs_exec
+        cur_jobs_exec = parent[cur_cost][cur_jobs_exec]
+        cur_cost = parent_cost[cur_cost][tmp_jobs_exec]
+    result.reverse()
+
+    return result
 
 
-# TODO: distinguish btwn sharemind and viff jobs
-def determineBackend(subdag):
-    nodes = subdag.nodes
-    coll_sets = set()
-    for node in nodes:
-        for col in node.columns:
-            coll_sets.add(col.collSets)
-    if len(coll_sets) > 1:
-        return 'sharemind'
-    else:
-        return 'spark'
+# TODO: implement s.t. cost is gauged for a node relative to particular framework
+def measureCost(nodes, fmwk):
+    return inf
 
 
 # calls appropriate codegen for a given job
