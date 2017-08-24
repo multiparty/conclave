fc48bf35e7a84351251a28f179b8060286d1db82
Ben Getchell <bengetch@gmail.com>
2017-08-24 13:28:42 -0400

    modify nielsen workflow

--- examples/nielsen/create_DFs.py
+++ examples/nielsen/create_DFs.py
@@ -1,6 +1,6 @@
 import salmon.lang as sal
 from salmon.comp import dagonly
-from salmon.codegen import spark, viz
+from salmon.codegen import spark
 from salmon.utils import *
 
 '''
@@ -13,26 +13,30 @@ code to iterate over files in a directory and concatenate the results along the 
 @dagonly
 def protocol():
 
+    # ['store_code_uc', 'upc', 'week_end', 'units', 'prmult', 'price', 'feature', 'display']
+
     # TODO: look at nielsen data and find appropriate column names / order
     colsInA = [
-        defCol("unit_price", "INTEGER", [1, 2, 3, 4, 5]),
-        defCol("price", "INTEGER", [1, 2, 3, 4, 5]),
-        defCol("prmult", "INTEGER", [1, 2, 3, 4, 5]),
-        defCol("store_code_uc", "INTEGER", [1, 2, 3, 4]),
-        defCol("week_end", "INTEGER", [1, 2, 3, 4, 5])
+        defCol("store_code_uc", "INTEGER", [1]),
+        defCol("upc", "INTEGER", [1]),
+        defCol("week_end", "INTEGER", [1]),
+        defCol("units", "INTEGER", [1]),
+        defCol("prmult", "INTEGER", [1]),
+        defCol("price", "FLOAT", [1]),
+        defCol("feature", "STRING", [1]),
+        defCol("display", "STRING", [1])
     ]
     create = sal.create("in1", colsInA, set([1]))
 
     w_unit_p = sal.divide(create, "w_unit_p", 'unit_price', ['price', 'prmult'])
     sum_units = sal.aggregate(w_unit_p, 'sum_units', ['store_code_uc', 'upc', 'week_end'], 'units', '+', 'q')
-    # TODO: what does our join do with duplicate columns?
-    total_units = sal.join(w_unit_p, sum_units, 'total_units' ['store_code_uc', 'upc', 'week_end'], \
+    total_units = sal.join(w_unit_p, sum_units, 'total_units', ['store_code_uc', 'upc', 'week_end'],
                            ['store_code_uc', 'upc', 'week_end'])
     wghtd_total = sal.multiply(total_units, 'wghtd_total', 'wghtd_unit_p', ['units', 'unit_price'])
     wghtd_total_final = sal.divide(wghtd_total, 'wghtd_total_final', 'wghtd_unit_p', ['wghtd_unit_p', 'q'])
-    total_unit_wghts = sal.aggregate(wghtd_total_final, 'total_unit_wghts', ['store_code_uc', 'upc', 'week_end'], \
+    total_unit_wghts = sal.aggregate(wghtd_total_final, 'total_unit_wghts', ['store_code_uc', 'upc', 'week_end'],
                                      'wghtd_unit_p', '+', 'avg_unit_p')
-    final_join = sal.join(total_units, total_unit_wghts, 'final_join', ['store_code_uc', 'upc', 'week_end'], \
+    final_join = sal.join(total_units, total_unit_wghts, 'final_join', ['store_code_uc', 'upc', 'week_end'],
                           ['store_code_uc', 'upc', 'week_end'])
 
     opened = sal.collect(final_join, 1)
---
+++ examples/nielsen/filter_products.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext, SparkConf
+import sys
+
+# filters products_cleaned_w_brand_code_bu.tsv
+
+conf = SparkConf().setAppName("create_div").setMaster("local")
+sc = SparkContext(conf=conf)
+
+col_ids = [4, 5, 8, 14]
+
+in_f = sc.textFile(sys.argv[1]) \
+    .map(lambda line: tuple(line.split('\t'))) \
+    .filter(lambda x: x[15] == 'OZ') \
+    .distinct() \
+    .map(lambda x: [x[i] for i in col_ids]) \
+    .saveAsTextFile(sys.argv[2])
