6bf5864ae1b6a87d049cb54eaa08b012739432b4
m1v0lg <n1v0lg@users.noreply.github.com>
2017-09-06 11:44:49 -0400

    Sharemind taxi cab wokflow

--- examples/dispatch/simpletaxi.py
+++ examples/dispatch/simpletaxi.py
@@ -34,16 +34,16 @@ def protocol():
     cab_data = sal.concat([cl1, cl2, cl3], "cab_data")
 
     selected_input = sal.project(cab_data, "selected_input", ["companyID", "price"])
-    local_rev = sal.aggregate(selected_input, "local_rev", "companyID", "price", "+", "local_rev")
+    local_rev = sal.aggregate(selected_input, "local_rev", ["companyID"], "price", "+", "local_rev")
     scaled_down = sal.divide(local_rev, "scaled_down", "local_rev", ["local_rev", 1000])
     first_val_blank = sal.multiply(scaled_down, "first_val_blank", "companyID", ["companyID", 0])
     local_rev_scaled = sal.multiply(first_val_blank, "local_rev_scaled", "local_rev", ["local_rev", 100])
-    total_rev = sal.aggregate(first_val_blank, "total_rev", "companyID", "local_rev", "+", "global_rev")
-    local_total_rev = sal.join(local_rev_scaled, total_rev, "local_total_rev", "companyID", "companyID")
+    total_rev = sal.aggregate(first_val_blank, "total_rev", ["companyID"], "local_rev", "+", "global_rev")
+    local_total_rev = sal.join(local_rev_scaled, total_rev, "local_total_rev", ["companyID"], ["companyID"])
     market_share = sal.divide(local_total_rev, "market_share", "local_rev", ["local_rev", "global_rev"])
     market_share_squared = sal.multiply(market_share, "market_share_squared", "local_rev",
                                         ["local_rev", "local_rev", 1])
-    hhi = sal.aggregate(market_share_squared, "hhi", "local_rev", "companyID", "+", "hhi")
+    hhi = sal.aggregate(market_share_squared, "hhi", ["companyID"], "local_rev", "+", "hhi")
 
     hhi_opened = sal._open(hhi, "hhi_opened", 1)
 
@@ -68,19 +68,14 @@ def party_proc(pid):
     salmon.dispatch.dispatch_all(peer, job_queue)
 
 if __name__ == "__main__":
-
-    sharemind_home = "/home/sharemind/Sharemind-SDK/sharemind/client"
-    job, code = SharemindCodeGen(protocol(), pid)._generate("job-1", sharemind_home)
-    print(code)
     
-
-    # # run each party in separate process
-    # # TODO: switch to threads if asyncio is thread-safe
-    # procs = []
-    # for pid in [1, 2, 3]:
-    #     p = Process(target=party_proc, args=(pid,))
-    #     p.start()
-    #     procs.append(p)
-    # # wait for processes to complete
-    # for p in procs:
-    #     p.join()
+    # run each party in separate process
+    # TODO: switch to threads if asyncio is thread-safe
+    procs = []
+    for pid in [1, 2, 3]:
+        p = Process(target=party_proc, args=(pid,))
+        p.start()
+        procs.append(p)
+    # wait for processes to complete
+    for p in procs:
+        p.join()
