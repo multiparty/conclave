bdfbd64745bdde93c5a904c0075174a43e759753
Ben Getchell <bengetch@gmail.com>
2017-09-20 21:35:53 -0400

    syntax

--- examples/codegen.py
+++ examples/codegen.py
@@ -7,18 +7,18 @@ def protocol():
 
     # define inputs
     colsInA = [
-        ("productID", "INTEGER", set([1, 2, 3])),
-        ("price", "INTEGER", set([1, 2, 3])),
-        ("amount", "INTEGER", set([1, 2, 3])),
-        ("userID", "INTEGER", set([1, 2, 3]))
+        ("productID", "INTEGER", set([1])),
+        ("price", "INTEGER", set([1])),
+        ("amount", "INTEGER", set([1])),
+        ("userID", "INTEGER", set([1]))
     ]
     inA = sal.create("inA", colsInA, set([1]))
 
     # specify the workflow
     proj = sal.project(inA, "projected", ["productID", "price", "amount"])
     mult = sal.multiply(proj, "subtotals", "price", ["price", "amount"])
-    agg = sal.aggregate(mult, "revenue", "productID", "price", "+", "revenue")
-    opened = sal.collect(mult, 1)
+    agg = sal.aggregate(mult, "revenue", ["productID"], "price", "+", "revenue")
+    opened = sal.collect(agg, 1)
 
     # return root nodes
     return set([inA])
