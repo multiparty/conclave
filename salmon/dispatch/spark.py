07a97790206c6685a2931e62b7985383c353448c
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-10-06 11:44:22 -0400

    Compose command line for Spark correctly
    Need to pass arguments as seperate array elements.

--- salmon/dispatch/spark.py
+++ salmon/dispatch/spark.py
@@ -10,14 +10,14 @@ class SparkDispatcher():
 
     def dispatch(self, job):
 
-        cmd = "{0}/{1}/bash.sh {2}"\
-            .format(job.root_dir, job.name, self.master)
+        cmd = "{0}/{1}/bash.sh"\
+            .format(job.root_dir, job.name)
 
         print("{}/{}/bash.sh dispatching to Spark master at {}"
               .format(job.root_dir, job.name, self.master)
               )
 
         try:
-            call(["/bin/bash", cmd])
+            call(["/bin/bash", cmd, self.master])
         except Exception as e:
             print(e)
