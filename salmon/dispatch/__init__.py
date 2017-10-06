e60790244b91db82aaac6512432c0043ca46a86d
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-10-06 11:45:58 -0400

    Actually allow `None` for dispatcher configs
    For convenience when not using Spark or Sharemind

--- salmon/dispatch/__init__.py
+++ salmon/dispatch/__init__.py
@@ -6,8 +6,8 @@ def dispatch_all(spark_master, sharemind_peer, jobs):
 
     # create a lookup from job class to instantiated dispatcher
     dispatchers = {
-        salmon.job.SharemindJob: sharemind.SharemindDispatcher(sharemind_peer),
-        salmon.job.SparkJob: spark.SparkDispatcher(spark_master)
+        salmon.job.SharemindJob: sharemind.SharemindDispatcher(sharemind_peer) if sharemind_peer else None,
+        salmon.job.SparkJob: spark.SparkDispatcher(spark_master) if spark_master else None
     }
 
     # dispatch each job
