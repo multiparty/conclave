419458f9511b16b2b09996215eb5478394b841d3
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-10-09 14:58:42 -0400

    Correct aggregation and join API in all examples
    For a while, our aggregations have taken lists of group columns and our
    joins have taken lists of join columns, but some examples hadn't been
    updated yet.
    
    Also fixes what I believe to be a correctness issue in taxi_workflow.py:
    the final aggregation ("hhi") was grouping by "local_rev" and adding
    "companyID", when it should have been the reverse. (@n1v0lg, @bengetch)

--- examples/workflow_with_datatypes.py
+++ examples/workflow_with_datatypes.py
@@ -21,8 +21,8 @@ def protocol():
     projStringInt2 = sal.project(multFloats, "projStringInt2", ["c", "d"])
     projStringFloat = sal.project(multFloats, "projStringFloat", ["a", "e"])
     projStringFloat2 = sal.project(multFloats, "projStringFloat2", ["c", "f"])
-    joinIntsOnString = sal.join(projStringInt, projStringInt2, "joinIntsOnString", "a", "c")
-    joinFloatsOnString = sal.join(projStringFloat, projStringFloat2, "joinFloatsOnString", "a", "c")
+    joinIntsOnString = sal.join(projStringInt, projStringInt2, "joinIntsOnString", ["a"], ["c"])
+    joinFloatsOnString = sal.join(projStringFloat, projStringFloat2, "joinFloatsOnString", ["a"], ["c"])
     # TODO(malte): this concats ints and float, which I guess we assume is fine, even though they
     # aren't the same column types?
     concat = sal.concat([joinIntsOnString, joinFloatsOnString], "concat")
