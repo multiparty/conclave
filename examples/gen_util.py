200143de2201cb8776aeaca63935c7142bab02fa
Ben Getchell <bengetch@gmail.com>
2017-10-19 15:08:12 -0400

    configurable header row

--- examples/gen_util.py
+++ examples/gen_util.py
@@ -2,26 +2,31 @@ import sys, os
 from random import randint
 
 
-def generate(out_path, num_cols, num_rows, col_names):
+def generate(out_path, num_cols, num_rows, col_names=None):
     num_cols = int(num_cols)
     num_rows = int(num_rows)
 
-    col_names = col_names.split(',')
-
-    assert (len(col_names) == num_cols), \
-        'Number of columns is unequal to number of column names.'
+    with_col_names = False
+    if col_names is not None:
+        with_col_names = True
+        col_names = col_names.split(',')
+        assert (len(col_names) == num_cols), \
+            'Number of columns is unequal to number of column names.'
 
     os.makedirs(os.path.dirname(out_path), exist_ok=True)
 
     with open(out_path, 'w') as f:
-
-        f.write(','.join(col_names) + '\n')
+        if with_col_names:
+            f.write(','.join(col_names) + '\n')
         f.write('\n'.join([','.join([str(randint(0,9))
                                      for i in range(num_cols)])
                            for j in range(num_rows)]))
 
 if __name__ == "__main__":
 
-    generate(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    try:
+        generate(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    except IndexError:
+        generate(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
