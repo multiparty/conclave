db9eb01e97609aa37be0d390e05c2a47ec0c3be7
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-10-24 14:41:03 -0400

    Make gen_data independent of $(PWD)

--- examples/benchmarks/sharemind/gen_data.sh
+++ examples/benchmarks/sharemind/gen_data.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 SIZES=(10 100 1000 10000 100000)
 PLAYERS=(1 2 3)
 
 for SIZE in ${SIZES[@]}; do
     for PLAYER in ${PLAYERS[@]}; do
-        python3 ../../gen_util.py /mnt/shared/$SIZE/in$PLAYER.csv 2 $SIZE "a,b"
+        python3 ${DIR}/../../gen_util.py /tmp/shared/$SIZE/in$PLAYER.csv 2 $SIZE "a,b"
     done
 done
