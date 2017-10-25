1917335a82d711b861009e20a2e25f937da0391e
Malte Schwarzkopf <malte@csail.mit.edu>
2017-10-25 23:31:33 +0000

    Minor fixes to SSN correctness checker script

--- examples/apps/ssn/check.sh
+++ examples/apps/ssn/check.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-python3 test.py > correct
-python3 sort.py /mnt/shared/ssn-1/sharemind-7/aggopened.csv > conclave
 
-if [[ $(diff conclave correct) -ne "" ]]; then
+if [[ $# -lt 1 ]]; then
+  echo "usage: check.sh <input file>"
+  exit 1
+fi
+
+python3 test.py > correct
+python3 sort.py $1 > conclave
+
+if [[ $(diff conclave correct) != "" ]]; then
   echo "ERROR: files are not identical! diff:"
   diff -u conclave correct 
 else
