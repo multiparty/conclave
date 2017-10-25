4103ad8b186cd88b848404c055b424d6d967e5e7
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-10-24 23:04:13 -0400

    Data generation script for SSN workflow
    Vary output scale:
    $ python gen_data.py --scale=N
    
    Simpler output:
    $ python gen_data.py --norealistic

---
+++ examples/apps/ssn/gen_data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import gflags, sys
+import random
+from faker import Faker
+
+FLAGS = gflags.FLAGS
+gflags.DEFINE_bool('realistic', True, 'Realistic output data.')
+gflags.DEFINE_integer('companies', 2, 'Number of credit card companies.')
+gflags.DEFINE_integer('scale', 1000, 'Scale factor.')
+gflags.DEFINE_string('output', 'ssn_data', 'Output file prefix.')
+
+def usage():
+    print('Usage: %s ARGS\\n%s' % (sys.argv[0], FLAGS))
+
+def main(argv):
+    try:
+        argv = FLAGS(argv)
+    except gflags.FlagsError as e:
+        print(e)
+        usage()
+        sys.exit(1)
+
+    f = Faker()
+    # regulator data: (name, address, zip, ssn)
+    rf = open("{}/regulator.csv".format(FLAGS.output), "w")
+    ssns = set()
+    for i in range(int(FLAGS.scale * 1.5)):
+        if FLAGS.realistic:
+            ssns.add(f.ssn())
+        else:
+            ssns.add(f.ssn().replace("-", ""))
+
+    people = set()
+    for i in range(FLAGS.scale):
+        ssn = random.sample(ssns, 1)[0]
+        while ssn in people:
+            ssn = random.sample(ssns, 1)[0]
+        people.add(ssn)
+        if FLAGS.realistic:
+            rf.write("\"{}\",\"{}\",{},{}\n".format(f.name(), f.street_address(), f.zipcode(), ssn))
+        else:
+            rf.write("{},{}\n".format(ssn, f.zipcode()))
+    rf.close()
+
+    # credit card company data
+    for c in range(FLAGS.companies):
+        ccf = open("{}/company{}.csv".format(FLAGS.output, c), "w")
+        for i in range(FLAGS.scale):
+            ssn = random.sample(ssns, 1)[0]
+            rating = 800
+            if FLAGS.realistic:
+                ccf.write("{},{},{}\n".format(ssn, f.credit_card_number(), rating))
+            else:
+                ccf.write("{},{}\n".format(ssn, rating))
+        ccf.close()
+
+if __name__ == '__main__':
+    main(sys.argv)
