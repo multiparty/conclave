b40f0bf6101dbe636fc215db13fe7d6413b66807
Ben <bengetch@gmail.com>
2018-01-12 10:56:06 -0500

    fix bash

--- examples/moc/run_moc.sh
+++ examples/moc/run_moc.sh
@@ -9,8 +9,6 @@ if [[ $# -lt 2 ]]; then
   exit 1
 fi
 
-hadoop fs -rm -r /home/ubuntu/taxi/local_rev*
-hadoop fs -rm -r /home/ubuntu/taxi/scaled_down*
-hadoop fs -rm -r /home/ubuntu/taxi/hhi_open*
+hadoop fs -rm -r /home/ubuntu/taxi/join2
 
 python3 ${DIR}/taxi.py $1 $2-spark-node-0:8020 /home/ubuntu spark://$2-spark-node-0:7077
