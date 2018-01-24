326c3201e9f097090e2dabce317eff38714fadaa
Ben <bengetch@gmail.com>
2018-01-24 16:33:39 -0500

    fix bench script

--- examples/gen_big.sh
+++ examples/gen_big.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo mkdir /mnt/taxi/15000000
+sudo mkdir /mnt/bench/150000000
 
-sudo python3 gen_util.py /mnt/taxi/150000000/in1 2 150000000 150000000 'a,b'
+sudo python3 gen_util.py /mnt/bench/150000000/in1 2 150000000 150000000 'a,b'
 
 hadoop fs -put /mnt/bench/150000000 ~/bench
 
-sudo rm -r /mnt/taxi/150000000
+sudo rm -r /mnt/bench/150000000
