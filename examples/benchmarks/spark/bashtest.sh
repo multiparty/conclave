aaa00693c4f49f0c6c0b49326552113e7ccd76bd
Ben <bengetch@gmail.com>
2018-01-24 16:57:27 -0500

    update bashtest.sh

--- examples/benchmarks/spark/bashtest.sh
+++ examples/benchmarks/spark/bashtest.sh
@@ -4,10 +4,10 @@ write_out () {
 start=$(date +%s)
 perflock python3 $1 ca-spark-node-0:8020 ~/bench/ $2 spark://ca-spark-node-0:7077
 end=$(date +%s)
-echo "runtime for $1 with $2 input rows was $((end - start))" >> bench.txt
+echo "runtime for $1 with $2 input rows was $((end - start))" >> bench150M.txt
 }
 
-sizes=( 30 300 3000 30000 300000 3000000 30000000 )
+sizes=( 150000000 )
 
 op=$1
 
