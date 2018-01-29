b497044f52ce8f199cf7d2e53949b3342edd1344
Ben <bengetch@gmail.com>
2018-01-29 10:23:31 -0500

    moc examples

--- examples/apps/moc/moc_example.py
+++ examples/apps/moc/moc_example.py
@@ -3,8 +3,7 @@ Simple example workflow for MOC deployment of Conclave
 """
 import conclave.lang as sal
 from conclave.utils import *
-from conclave.codegen.spark import SparkConfig
-from conclave import CodeGenConfig
+from conclave.config import SharemindCodeGenConfig, SparkConfig, CodeGenConfig, NetworkConfig
 from conclave import generate_and_dispatch
 import sys
 
@@ -48,42 +47,22 @@ def protocol():
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 5:
-        print("usage: moc_example.py <party_id> <HDFS master node:port> <HDFS root dir> <Spark master url>")
-        sys.exit(1)
-
     pid = int(sys.argv[1])
-    hdfs_namenode = sys.argv[2]
-    hdfs_root = sys.argv[3]
-    spark_master_url = sys.argv[4]
 
-    workflow_name = "job-" + str(pid)
+    net_conf = NetworkConfig(
+        ["ca-spark-node-0", "cb-spark-node-0", "cc-spark-node-0"],
+        [8020, 8020, 8020],
+        1
+    )
 
-    network_config = {
-        "pid": pid,
-        "parties": {
-            1: {"host": "ca-spark-node-0", "port": 9001},
-            2: {"host": "cb-spark-node-0", "port": 9002},
-            3: {"host": "cc-spark-node-0", "port": 9003}
-        }
-    }
+    spark_conf = SparkConfig("spark://ca-spark-node-0:8020")
+    sharemind_conf = SharemindCodeGenConfig("/mnt/data")
+    conclave_config = CodeGenConfig("big_job"). \
+        with_sharemind_config(sharemind_conf). \
+        with_spark_config(spark_conf). \
+        with_network_config(net_conf)
 
-    # init spark config
-    spark_config = SparkConfig(spark_master_url)
-
-    # add spark config and network config to master config
-    conclave_config = CodeGenConfig(workflow_name) \
-        .with_spark_config(spark_config)
-    conclave_config.with_network_config(network_config)
-
-    # add code path, input directory path, output directory path, pid, and name
-    conclave_config.code_path = "/mnt/shared/" + workflow_name
-    conclave_config.input_path = "hdfs://{}/{}/taxi".format(
-        hdfs_namenode, hdfs_root)
-    conclave_config.output_path = "hdfs://{}/{}/taxi".format(
-        hdfs_namenode, hdfs_root)
     conclave_config.pid = pid
-    conclave_config.name = workflow_name
 
     # run the protocol
     generate_and_dispatch(
