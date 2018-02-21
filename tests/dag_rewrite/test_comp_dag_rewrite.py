578a7a511b4398583099bafd3ebdd858b233e0aa
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-02-21 20:13:22 +0100

    Update ssn test

--- tests/dag_rewrite/test_comp_dag_rewrite.py
+++ tests/dag_rewrite/test_comp_dag_rewrite.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-import conclave.lang as sal
+import conclave.lang as cc
 from conclave.comp import mpc, scotch
 from conclave.utils import *
 import os
@@ -26,20 +26,20 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            in_1 = sal.create("in1", cols_in_1, {1})
+            in_1 = cc.create("in1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("a", "INTEGER", [2]),
                 defCol("b", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
 
             # combine parties' inputs into one relation
-            rel = sal.concat([in_1, in_2], "rel")
+            rel = cc.concat([in_1, in_2], "rel")
 
             # specify the workflow
-            mult = sal.multiply(rel, "mult", "a", ["a", 0])
+            mult = cc.multiply(rel, "mult", "a", ["a", 0])
 
-            sal.collect(mult, 1)
+            cc.collect(mult, 1)
 
             # return root nodes
             return {in_1, in_2}
@@ -58,24 +58,24 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("a", "INTEGER", [2]),
                 defCol("b", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
             cols_in_3 = [
                 defCol("a", "INTEGER", [3]),
                 defCol("b", "INTEGER", [3])
             ]
-            in_3 = sal.create("in_3", cols_in_3, {3})
+            in_3 = cc.create("in_3", cols_in_3, {3})
 
             # combine parties' inputs into one relation
-            rel = sal.concat([in_1, in_2, in_3], "rel")
-            proj = sal.project(rel, "proj", ["a", "b"])
-            agg = sal.aggregate(proj, "agg", ["a"], "b", "+", "total_b")
+            rel = cc.concat([in_1, in_2, in_3], "rel")
+            proj = cc.project(rel, "proj", ["a", "b"])
+            agg = cc.aggregate(proj, "agg", ["a"], "b", "+", "total_b")
 
-            sal.collect(agg, 1)
+            cc.collect(agg, 1)
 
             # return root nodes
             return {in_1, in_2, in_3}
@@ -94,23 +94,23 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("a", "INTEGER", [2]),
                 defCol("b", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
 
             # combine parties' inputs into one relation
-            rel = sal.concat([in_1, in_2], "rel")
+            rel = cc.concat([in_1, in_2], "rel")
 
             # specify the workflow
-            proj_a = sal.project(rel, "proj_a", ["a", "b"])
-            proj_b = sal.project(proj_a, "proj_b", ["a", "b"])
-            agg = sal.aggregate(proj_b, "agg", ["a"], "b", "+", "total_b")
-            proj_c = sal.project(agg, "proj_c", ["a", "total_b"])
+            proj_a = cc.project(rel, "proj_a", ["a", "b"])
+            proj_b = cc.project(proj_a, "proj_b", ["a", "b"])
+            agg = cc.aggregate(proj_b, "agg", ["a"], "b", "+", "total_b")
+            proj_c = cc.project(agg, "proj_c", ["a", "total_b"])
 
-            sal.collect(proj_c, 1)
+            cc.collect(proj_c, 1)
 
             # return root nodes
             return {in_1, in_2}
@@ -127,17 +127,17 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1]),
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
 
             cols_in_2 = [
                 defCol("c", "INTEGER", [2]),
                 defCol("d", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
-            proj_b = sal.project(in_2, "proj_b", ["c", "d"])
+            in_2 = cc.create("in_2", cols_in_2, {2})
+            proj_b = cc.project(in_2, "proj_b", ["c", "d"])
 
-            joined = sal.join(in_1, proj_b, "joined", ["a"], ["c"])
-            sal.collect(joined, 1)
+            joined = cc.join(in_1, proj_b, "joined", ["a"], ["c"])
+            cc.collect(joined, 1)
             return {in_1, in_2}
 
         actual = protocol()
@@ -152,13 +152,13 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("a", "INTEGER", [1], [2]),
                 defCol("b", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
-            sal.collect(sal.aggregate(sal.concat([in_1, in_2], "rel"), "agg", ["a"], "b", "+", "total_b"), 1)
+            in_2 = cc.create("in_2", cols_in_2, {2})
+            cc.collect(cc.aggregate(cc.concat([in_1, in_2], "rel"), "agg", ["a"], "b", "+", "total_b"), 1)
             return {in_1, in_2}
 
         actual = protocol()
@@ -174,17 +174,17 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1]),
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
 
             cols_in_2 = [
                 defCol("c", "INTEGER", [1], [2]),
                 defCol("d", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
 
-            result = sal.join(in_1, in_2, "result", ["a"], ["c"])
+            result = cc.join(in_1, in_2, "result", ["a"], ["c"])
 
-            sal.collect(result, 1)
+            cc.collect(result, 1)
             # create dag
             return {in_1, in_2}
 
@@ -201,17 +201,17 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1], [2]),
                 defCol("b", "INTEGER", [1]),
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
 
             cols_in_2 = [
                 defCol("c", "INTEGER", [2]),
                 defCol("d", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
 
-            result = sal.join(in_1, in_2, "result", ["a"], ["c"])
+            result = cc.join(in_1, in_2, "result", ["a"], ["c"])
 
-            sal.collect(result, 1)
+            cc.collect(result, 1)
             # create dag
             return {in_1, in_2}
 
@@ -227,23 +227,28 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            govreg = sal.create("a_govreg", govreg_cols, {1})
-            govreg_dummy = sal.project(govreg, "govreg_dummy", ["a", "b"])
+            govreg = cc.create("a_govreg", govreg_cols, {1})
+            govreg_dummy = cc.project(govreg, "govreg_dummy", ["a", "b"])
+
             company0_cols = [
                 defCol("c", "INTEGER", [1], [2]),
-                defCol("d", "INTEGER", [1])
+                defCol("d", "INTEGER", [2])
             ]
-            company0 = sal.create("company0", company0_cols, {2})
+            company0 = cc.create("company0", company0_cols, {2})
+            company0_dummy = cc.project(company0, "company0_dummy", ["c", "d"])
+
             company1_cols = [
                 defCol("c", "INTEGER", [1], [3]),
-                defCol("d", "INTEGER", [1])
+                defCol("d", "INTEGER", [3])
             ]
-            company1 = sal.create("company1", company1_cols, {3})
-            companies = sal.concat([company0, company1], "companies")
+            company1 = cc.create("company1", company1_cols, {3})
+            company1_dummy = cc.project(company1, "company1_dummy", ["c", "d"])
 
-            joined = sal.join(govreg_dummy, companies, "joined", ["a"], ["c"])
-            expected = sal.aggregate(joined, "expected", ["b"], "d", "+", "total")
-            sal.collect(expected, 1)
+            companies = cc.concat([company0_dummy, company1_dummy], "companies")
+
+            joined = cc.join(govreg_dummy, companies, "joined", ["a"], ["c"])
+            actual = cc.aggregate(joined, "actual", ["b"], "d", "+", "total")
+            cc.collect(actual, 1)
 
             return {govreg, company0, company1}
 
@@ -260,42 +265,42 @@ class TestConclave(TestCase):
                 defCol("companyID", "INTEGER", [1]),
                 defCol("price", "INTEGER", [1])
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("companyID", "INTEGER", [2]),
                 defCol("price", "INTEGER", [2])
             ]
-            in_2 = sal.create("in_2", cols_in_2, {2})
+            in_2 = cc.create("in_2", cols_in_2, {2})
             cols_in_3 = [
                 defCol("companyID", "INTEGER", [3]),
                 defCol("price", "INTEGER", [3])
             ]
-            in_3 = sal.create("in_3", cols_in_3, {3})
+            in_3 = cc.create("in_3", cols_in_3, {3})
 
-            cab_data = sal.concat([in_1, in_2, in_3], "cab_data")
+            cab_data = cc.concat([in_1, in_2, in_3], "cab_data")
 
-            selected_input = sal.project(
+            selected_input = cc.project(
                 cab_data, "selected_input", ["companyID", "price"])
-            local_rev = sal.aggregate(selected_input, "local_rev", [
+            local_rev = cc.aggregate(selected_input, "local_rev", [
                 "companyID"], "price", "+", "local_rev")
-            scaled_down = sal.divide(
+            scaled_down = cc.divide(
                 local_rev, "scaled_down", "local_rev", ["local_rev", 1000])
-            first_val_blank = sal.multiply(
+            first_val_blank = cc.multiply(
                 scaled_down, "first_val_blank", "companyID", ["companyID", 0])
-            local_rev_scaled = sal.multiply(
+            local_rev_scaled = cc.multiply(
                 first_val_blank, "local_rev_scaled", "local_rev", ["local_rev", 100])
-            total_rev = sal.aggregate(first_val_blank, "total_rev", [
+            total_rev = cc.aggregate(first_val_blank, "total_rev", [
                 "companyID"], "local_rev", "+", "global_rev")
-            local_total_rev = sal.join(local_rev_scaled, total_rev, "local_total_rev", [
+            local_total_rev = cc.join(local_rev_scaled, total_rev, "local_total_rev", [
                 "companyID"], ["companyID"])
-            market_share = sal.divide(local_total_rev, "market_share", "local_rev", [
+            market_share = cc.divide(local_total_rev, "market_share", "local_rev", [
                 "local_rev", "global_rev"])
-            market_share_squared = sal.multiply(market_share, "market_share_squared", "local_rev",
-                                                ["local_rev", "local_rev", 1])
-            hhi = sal.aggregate(market_share_squared, "hhi", [
+            market_share_squared = cc.multiply(market_share, "market_share_squared", "local_rev",
+                                               ["local_rev", "local_rev", 1])
+            hhi = cc.aggregate(market_share_squared, "hhi", [
                 "companyID"], "local_rev", "+", "hhi")
 
-            sal.collect(hhi, 1)
+            cc.collect(hhi, 1)
 
             # return root nodes
             return {in_1, in_2, in_3}
@@ -314,26 +319,26 @@ class TestConclave(TestCase):
                 defCol("a", "INTEGER", [1]),
                 defCol("b", "INTEGER", [1])
             ]
-            in_1 = sal.create("in_1", cols_in_1, {1})
+            in_1 = cc.create("in_1", cols_in_1, {1})
             cols_in_2 = [
                 defCol("a", "INTEGER", [2]),
                 defCol("b", "INTEGER", [2])
             ]
-            in_2 = sal.create("in2", cols_in_2, {2})
+            in_2 = cc.create("in2", cols_in_2, {2})
             cols_in_3 = [
                 defCol("a", "INTEGER", [3]),
                 defCol("b", "INTEGER", [3])
             ]
-            in_3 = sal.create("in_3", cols_in_3, {3})
+            in_3 = cc.create("in_3", cols_in_3, {3})
 
             # combine parties' inputs into one relation
-            rel = sal.concat([in_1, in_2, in_3], "rel")
-            proj = sal.project(rel, "proj", ["a", "b"])
-            agg = sal.aggregate(proj, "agg", ["a"], "b", "+", "total_b")
-            div = sal.divide(agg, "div", "a", ["a", 1])
-            mult = sal.multiply(div, "mult", "a", ["a", 1])
+            rel = cc.concat([in_1, in_2, in_3], "rel")
+            proj = cc.project(rel, "proj", ["a", "b"])
+            agg = cc.aggregate(proj, "agg", ["a"], "b", "+", "total_b")
+            div = cc.divide(agg, "div", "a", ["a", 1])
+            mult = cc.multiply(div, "mult", "a", ["a", 1])
 
-            sal.collect(mult, 1)
+            cc.collect(mult, 1)
 
             # return root nodes
             return {in_1, in_2, in_3}
