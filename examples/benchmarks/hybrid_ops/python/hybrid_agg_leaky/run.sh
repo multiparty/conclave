1acc43d9802db5729bcc1a4a02fc28fbaf8e7c33
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-02-24 19:38:35 +0100

    Fix path

--- examples/benchmarks/hybrid_ops/python/hybrid_agg_leaky/run.sh
+++ examples/benchmarks/hybrid_ops/python/hybrid_agg_leaky/run.sh
@@ -3,7 +3,7 @@
 PARTY_ID=$1
 DATA_ROOT_DIR=$2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=${HOME}/Desktop/work/conclave
+export PYTHONPATH=${HOME}/conclave
 
 # clean up data first
 find /mnt/shared/${DATA_ROOT_DIR} -type f -not -name in1.csv -not -name in2.csv -not -name in3.csv -print0 | xargs -0 rm --
