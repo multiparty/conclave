8458f6af242dfcb7df8694feab21a052552cc0d0
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-02-25 15:45:14 +0100

    More

--- examples/benchmarks/hybrid_ops/python/hybrid_agg_leaky/run_all.sh
+++ examples/benchmarks/hybrid_ops/python/hybrid_agg_leaky/run_all.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 PARTY_ID=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=${HOME}/conclave
-./run.sh "${PARTY_ID}" "hybrid_agg_data/10"
-./run.sh "${PARTY_ID}" "hybrid_agg_data/100"
+bash ${DIR}/run.sh "${PARTY_ID}" "hybrid_agg_data/10"
+bash ${DIR}/run.sh "${PARTY_ID}" "hybrid_agg_data/100"
 #for size in 10 100 1000;
 #do
 #    bash run.sh ${PARTY_ID} hybrid_agg_data/${size};
