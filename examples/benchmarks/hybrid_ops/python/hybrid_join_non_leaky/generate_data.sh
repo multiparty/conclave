773b0b0dfc78d524a0ff91d91cbadd9a381feeae
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-02-26 11:04:48 +0100

    Non-leaky hybrid join benchmark

---
+++ examples/benchmarks/hybrid_ops/python/hybrid_join_non_leaky/generate_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PYTHONPATH=${HOME}/conclave
+CC=${HOME}/conclave
+for size in 10 100 1000 10000 100000 1000000;
+do
+    for party in 1 2;
+        do
+            OUT=/mnt/shared/hybrid_join_data/${size}/in${party}.csv
+            python3 ${CC}/examples/gen_util.py ${OUT} 2 ${size} ${size} ${party} 0
+        done
+done
