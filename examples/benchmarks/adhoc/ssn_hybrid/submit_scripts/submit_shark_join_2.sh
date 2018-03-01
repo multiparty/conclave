6a379a0d79407e9146fa844525efe77157418ece
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-03-01 18:59:42 +0100

    Hybrid agg reveal keys

--- examples/benchmarks/adhoc/ssn_hybrid/submit_scripts/submit_shark_join_2.sh
+++ examples/benchmarks/adhoc/ssn_hybrid/submit_scripts/submit_shark_join_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /mnt/shared/
 ./sm_compile_to_remote.sh /mnt/shared/ssn_hybrid/secrec/shark_join_2.sc
-./Submitter --bytecode shark_join_2.sb --rels-meta joined:3 --output-path /mnt/shared/ssn_hybrid/data
+./Submitter --bytecode shark_join_2.sb --rels-meta group_by_keys:1 --output-path /mnt/shared/ssn_hybrid/data
