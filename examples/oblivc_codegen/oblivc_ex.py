4fb8558b00eff3c293ce8e03310eed37823cec4e
Ben <bengetch@gmail.com>
2018-03-13 16:51:26 -0400

    test codegen changes

--- examples/oblivc_codegen/oblivc_ex.py
+++ examples/oblivc_codegen/oblivc_ex.py
@@ -16,13 +16,15 @@ def generate(dag):
 
     cfg.with_oc_config(oc_conf)
 
-    cg = OblivcCodeGen(cfg, dag, 1)
+    cg1 = OblivcCodeGen(cfg, dag, 1)
+    cg1.generate('protocol1', '/tmp')
 
-    cg.generate('oc_join', '/tmp')
+    cg2 = OblivcCodeGen(cfg, dag, 2)
+    cg2.generate('protocol2', '/tmp')
 
 
 @dag_only
-def proj():
+def agg():
 
     colsInA = [
         defCol('a', 'INTEGER', [1]),
@@ -41,15 +43,19 @@ def proj():
     in1 = sal.create("in1", colsInA, set([1]))
     in2 = sal.create("in2", colsInB, set([2]))
 
-    cl1 = sal._close(in1, "cl1", set([1, 2, 3]))
-    cl2 = sal._close(in2, "cl2", set([1, 2, 3]))
+    cl1 = sal._close(in1, "cl1", set([1, 2]))
+    cl2 = sal._close(in2, "cl2", set([1, 2]))
 
     rel = sal.concat([cl1, cl2], "rel")
+
+    agg = sal.aggregate(rel, 'aggOne', ['c'], 'a', '+', 'c_agg')
+
+    open = sal._open(agg, "opened", 1)
 
     return set([in1, in2])
 
 
 if __name__ == "__main__":
 
-    dag = proj()
+    dag = agg()
     generate(dag)
