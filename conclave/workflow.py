05fd9c6786430b87e4839c2f234e05f8fcba6d59
Ben <bengetch@gmail.com>
2018-04-08 11:33:25 -0400

    clean up workflow

--- conclave/workflow.py
+++ conclave/workflow.py
@@ -4,7 +4,6 @@ from typing import Callable, Dict
 
 from conclave import generate_and_dispatch
 from conclave import CodeGenConfig
-from conclave.config import SharemindCodeGenConfig
 from conclave.config import SparkConfig
 from conclave.config import OblivcConfig
 from conclave.config import NetworkConfig
@@ -26,10 +25,6 @@ def setup(conf: Dict):
     spark_master_url = conf["spark"]["master_url"]
     spark_config = SparkConfig(spark_master_url)
 
-    # TODO: deprecated, not using SM anymore.
-    # SHAREMIND
-    sm_config = SharemindCodeGenConfig(conf["code_path"])
-
     # OBLIV-C
     oc_path = conf["oblivc"]["oc_path"]
     ip_port = conf["oblivc"]["ip_port"]
@@ -42,7 +37,6 @@ def setup(conf: Dict):
 
     # CONCLAVE SYSTEM CONFIG
     conclave_config = CodeGenConfig(workflow_name) \
-        .with_sharemind_config(sm_config) \
         .with_spark_config(spark_config) \
         .with_oc_config(oc_config) \
         .with_swift_config(swift_config) \
@@ -51,10 +45,9 @@ def setup(conf: Dict):
     conclave_config.pid = pid
     conclave_config.name = workflow_name
 
-    # TODO hardcode these three lines, will be constant on OS containers.
-    conclave_config.code_path = "{0}/{1}".format(conf["code_path"], workflow_name)
-    conclave_config.input_path = "{0}/{1}-in/".format(conf["input_path"], conf["name"])
-    conclave_config.output_path = "{0}/{1}-out/".format(conf["output_path"], conf["name"])
+    conclave_config.code_path = "/tmp/{0}-code/".format(workflow_name)
+    conclave_config.input_path = "/tmp/{0}-in/".format(conf["name"])
+    conclave_config.output_path = "/tmp/{0}-out/".format(conf["name"])
 
     return conclave_config
 
