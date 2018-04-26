88e3df705f9ea13dd3e1ec23e1f7fabb4475587c
Ben <bengetch@gmail.com>
2018-04-26 12:53:35 -0400

    rm hadoop command for oc datagen

--- examples/gen_oc_data.sh
+++ examples/gen_oc_data.sh
@@ -3,7 +3,6 @@
 write_data () {
 sudo mkdir /mnt/bench/$1
 sudo python3 gen_util.py /mnt/bench/$1/in1 4 $1 $1 'a,b'
-hadoop fs -put /mnt/bench/$1 ~/bench/
 }
 
 sizes=( 10 100 200 300 400 500 600 700 800 900 )
