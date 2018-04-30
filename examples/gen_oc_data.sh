c55791ad110cef12017bf55084e20691ca482671
Ben <bengetch@gmail.com>
2018-04-30 14:44:23 -0400

    modift gen scripts

--- examples/gen_oc_data.sh
+++ examples/gen_oc_data.sh
@@ -5,7 +5,7 @@ sudo mkdir /mnt/bench/$1
 sudo python3 gen_util.py /mnt/bench/$1/in1 2 $1 $1 'a,b'
 }
 
-sizes=( 2 4 8 16 32 64 128 256 512 1028 2056 )
+sizes=( 2 4 8 16 32 64 128 256 512 1024 2048 )
 
 sudo mkdir /mnt/bench
 
