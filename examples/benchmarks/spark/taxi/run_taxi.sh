a63b5a013d5ccdfd79da7d8630ea8e6b497146fb
Ben <bengetch@gmail.com>
2018-05-03 10:48:09 -0400

    filepaths

--- examples/benchmarks/spark/taxi/run_taxi.sh
+++ examples/benchmarks/spark/taxi/run_taxi.sh
@@ -5,9 +5,9 @@ export PYTHONPATH=${HOME}/conclave
 export PATH=${PATH}:${HOME}/spark-2.2.0-bin-hadoop2.6/bin
 
 run_bench () {
-    sudo python3 gen_data.py /mnt/taxi/bench/ $1 1
-    sudo python3 gen_data.py /mnt/taxi/bench/ $1 2
-    sudo python3 gen_data.py /mnt/taxi/bench/ $1 3
+    sudo python3 gen_data.py /mnt/taxi/bench/yellow1.csv $1 1
+    sudo python3 gen_data.py /mnt/taxi/bench/yellow2.csv $1 2
+    sudo python3 gen_data.py /mnt/taxi/bench/yellow3.csv $1 3
     hadoop fs -put /mnt/taxi/bench/* ~/taxi/
 
     sudo rm -r /mnt/taxi/bench/*
