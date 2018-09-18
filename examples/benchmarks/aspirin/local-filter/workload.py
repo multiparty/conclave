4e829a82f43e4f958ed222a7278cdc246610a4d9
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-09-18 19:13:00 +0200

    Aspirin run scripts

--- examples/benchmarks/aspirin/local-filter/workload.py
+++ examples/benchmarks/aspirin/local-filter/workload.py
@@ -16,12 +16,12 @@ def protocol():
     diag_col_diags = "16"
     date_col_diags = "18"
 
-    num_med_cols = 8 
+    num_med_cols = 8
     medication_cols = [defCol(str(i), "INTEGER", [1]) for i in range(num_med_cols)]
 
     medication = cc.create("medication", medication_cols, {1})
 
-    num_diag_cols = 13 
+    num_diag_cols = 13
     diagnosis_cols = [defCol(str(i + num_med_cols), "INTEGER", [2]) for i in range(num_diag_cols)]
 
     diagnosis = cc.create("diagnosis", diagnosis_cols, {2})
@@ -29,7 +29,7 @@ def protocol():
     # only keep relevant columns
     medication_proj = cc.project(medication, "medication_proj", [pid_col_meds, med_col_meds, date_col_meds])
     diagnosis_proj = cc.project(diagnosis, "diagnosis_proj", [pid_col_diags, diag_col_diags, date_col_diags])
-    
+
     aspirin = cc.cc_filter(medication_proj, "aspirin", med_col_meds, "==", scalar=1)
     heart_patients = cc.cc_filter(diagnosis_proj, "heart_patients", diag_col_diags, "==", scalar=1)
 
@@ -43,26 +43,25 @@ def protocol():
 
 if __name__ == "__main__":
     pid = sys.argv[1]
-    try:
-        use_leaky = sys.argv[2] == "-l"
-    except Exception:
-        use_leaky = False
+    data_root = sys.argv[2]
     # define name for the workflow
-    workflow_name = "real-aspirin-test-" + pid
+    workflow_name = "aspirin-local-filter-" + pid + "-" + data_root
     # configure conclave
     conclave_config = CodeGenConfig(workflow_name, int(pid))
-    conclave_config.all_pids = [1, 2, 3]
-    conclave_config.use_leaky_ops = use_leaky
-    sharemind_conf = SharemindCodeGenConfig("/mnt/shared", 
-        use_docker=True,
-        use_hdfs=False)
+    conclave_config.network_config = {
+        "pid": int(pid),
+        "parties": {
+            1: {"host": "ca-spark-node-0", "port": 9001},
+            2: {"host": "cb-spark-node-0", "port": 9002},
+            3: {"host": "cc-spark-node-0", "port": 9003}
+        }
+    }
+    conclave_config.use_leaky_ops = False
+    sharemind_conf = SharemindCodeGenConfig("/mnt/shared", use_docker=True, use_hdfs=False)
     conclave_config.with_sharemind_config(sharemind_conf)
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    # point conclave to the directory where the generated code should be stored/ read from
     conclave_config.code_path = os.path.join("/mnt/shared", workflow_name)
-    # point conclave to directory where data is to be read from...
-    conclave_config.input_path = os.path.join(current_dir, "data")
-    # and written to
-    conclave_config.output_path = os.path.join(current_dir, "data")
+    conclave_config.input_path = os.path.join("/mnt/shared", data_root)
+    conclave_config.output_path = os.path.join("/mnt/shared", data_root)
+
     job_queue = generate_code(protocol, conclave_config, ["sharemind"], ["python"], apply_optimizations=True)
     dispatch_jobs(job_queue, conclave_config)
