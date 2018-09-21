f48b77edfb89a613672d04bbad2bb84b0985f395
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-09-21 18:19:35 +0200

    More runs

--- examples/benchmarks/aspirin/public-join/generate_data.sh
+++ examples/benchmarks/aspirin/public-join/generate_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for size in 10 100 1000 10000;
+for size in 10 100 1000 10000 100000 500000;
 do
     OUT=/mnt/shared/aspirin_data/${size}/;
     python3 gen_data.py -n ${size} -d ${size} -r 0.2 -o ${OUT};
