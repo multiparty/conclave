0dba1674fa447d2e1982c8aabb1ac2d073aae309
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-10-18 14:44:18 +0200

    Bring sharemind ssn up to date

--- examples/apps/ssn/sharemind/sharemind_ssn.py
+++ examples/apps/ssn/sharemind/sharemind_ssn.py
@@ -1,70 +1,61 @@
-from conclave.codegen.sharemind import SharemindCodeGen, SharemindCodeGenConfig
-from conclave import CodeGenConfig
-import conclave.dispatch
-import conclave.net
-from conclave.comp import dag_only
-import conclave.lang as sal
-from conclave.utils import *
+import os
 import sys
 
-@dag_only
-def protocol():
+import conclave.dag as ccdag
+import conclave.lang as cc
+from conclave import dispatch_jobs, SharemindCodeGen
+from conclave.config import CodeGenConfig, SharemindCodeGenConfig
+from conclave.utils import *
 
+
+def protocol():
     # define inputs
     colsIn1 = [
         defCol("a", "INTEGER", [1]),
         defCol("b", "INTEGER", [1])
     ]
-    in1 = sal.create("govreg", colsIn1, set([1]))
+    in1 = cc.create("govreg", colsIn1, {1})
     colsIn2 = [
         defCol("c", "INTEGER", [2]),
         defCol("d", "INTEGER", [2])
     ]
-    in2 = sal.create("company0", colsIn2, set([2]))
+    in2 = cc.create("company0", colsIn2, {2})
     colsIn3 = [
         defCol("c", "INTEGER", [3]),
         defCol("d", "INTEGER", [3])
     ]
-    in3 = sal.create("company1", colsIn3, set([3]))
+    in3 = cc.create("company1", colsIn3, {3})
 
-    cl1 = sal._close(in1, "cl1", set([1, 2, 3]))
-    projA = sal.project(cl1, "projA", ["a", "b"])
-    cl2 = sal._close(in2, "cl2", set([1, 2, 3]))
-    cl3 = sal._close(in3, "cl3", set([1, 2, 3]))
-    right_rel = sal.concat([cl2, cl3], "right_rel")
-    projB = sal.project(right_rel, "projB", ["c", "d"])
+    cl1 = cc._close(in1, "cl1", {1, 2, 3})
+    projA = cc.project(cl1, "projA", ["a", "b"])
+    cl2 = cc._close(in2, "cl2", {1, 2, 3})
+    cl3 = cc._close(in3, "cl3", {1, 2, 3})
+    right_rel = cc.concat([cl2, cl3], "right_rel")
+    projB = cc.project(right_rel, "projB", ["c", "d"])
 
-    joined = sal.join(projA, right_rel, "joined", ["a"], ["c"])
-    agg = sal.aggregate(joined, "agg", ["b"], "d", "+", "total")
+    joined = cc.join(projA, projB, "joined", ["a"], ["c"])
+    agg = cc.aggregate(joined, "agg", ["b"], "d", "+", "total")
 
-    opened = sal._open(agg, "opened", 1)
-    return set([in1, in2, in3])
+    cc._open(agg, "opened", 1)
+    return {in1, in2, in3}
+
 
 if __name__ == "__main__":
-
-    pid = int(sys.argv[1])
-    
-    workflow_name = "sharemind-ssn-" + str(pid)
-    sm_cg_config = SharemindCodeGenConfig(
-        workflow_name, "/mnt/shared", use_hdfs=False, use_docker=True)
-    codegen_config = CodeGenConfig(
-        workflow_name).with_sharemind_config(sm_cg_config)
-    codegen_config.code_path = "/mnt/shared/" + workflow_name
-    codegen_config.input_path = "/mnt/shared/ssn-data"
-    codegen_config.output_path = "/mnt/shared/ssn-data"
-
-    job = SharemindCodeGen(codegen_config, protocol(), pid).generate(
-        "sharemind-0", "")
+    pid = sys.argv[1]
+    # define name for the workflow
+    workflow_name = "sharemind-ssn-" + pid
+    # configure conclave
+    conclave_config = CodeGenConfig(workflow_name, int(pid))
+    conclave_config.all_pids = [1, 2, 3]
+    sharemind_conf = SharemindCodeGenConfig("/mnt/shared", use_docker=True, use_hdfs=False)
+    conclave_config.with_sharemind_config(sharemind_conf)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    # point conclave to the directory where the generated code should be stored/ read from
+    conclave_config.code_path = os.path.join("/mnt/shared", workflow_name)
+    # point conclave to directory where data is to be read from...
+    conclave_config.input_path = "/mnt/shared/ssn_data"
+    # and written to
+    conclave_config.output_path = "/mnt/shared/ssn_data"
+    job = SharemindCodeGen(conclave_config, ccdag.OpDag(protocol()), conclave_config.pid).generate("sharemind-0", "")
     job_queue = [job]
-    
-    sharemind_config = {
-        "pid": pid,
-        "parties": {
-            1: {"host": "ca-spark-node-0", "port": 9001},
-            2: {"host": "cb-spark-node-0", "port": 9002},
-            3: {"host": "cc-spark-node-0", "port": 9003}
-        }
-    }
-    sm_peer = conclave.net.setup_peer(sharemind_config)
-    conclave.dispatch.dispatch_all(None, sm_peer, job_queue)
-
+    dispatch_jobs(job_queue, conclave_config)
