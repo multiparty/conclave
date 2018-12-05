63f56cdbb3e27c79050b0060935c509febb8656d
Ben <bengetch@gmail.com>
2018-12-05 14:12:59 -0500

    use json instead of yaml for cc config.
    made dateverse vs swift vs local storage configurable
    
    todo: dataverse class for pulling // posting data

--- conclave/workflow.py
+++ conclave/workflow.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 import argparse
 import os
 from typing import Callable, Dict
@@ -16,29 +17,25 @@ from conclave.swift import SwiftData
 def setup(conf: Dict):
 
     # GENERAL
-    pid = conf["pid"]
-    workflow_name = conf["workflow_name"]
-    use_swift = conf["use_swift"]
-    all_pids = conf['all_pids']
-
-    # SWIFT
-    cfg = conf["swift"]
-    swift_config = SwiftConfig(cfg)
+    pid = conf["user_config"]["pid"]
+    workflow_name = conf["user_config"]["workflow_name"]
+    data_backend = conf["data"]["data_backend"]
+    all_pids = conf["user_config"]['all_pids']
 
     # SPARK
-    spark_master_url = conf["spark"]["master_url"]
+    spark_master_url = conf["backends"]["spark"]["master_url"]
     spark_config = SparkConfig(spark_master_url)
 
     # OBLIV-C
-    oc_path = conf["oblivc"]["oc_path"]
-    ip_port = conf["oblivc"]["ip_port"]
+    oc_path = conf["backends"]["oblivc"]["oc_path"]
+    ip_port = conf["backends"]["oblivc"]["ip_port"]
     oc_config = OblivcConfig(oc_path, ip_port)
 
     # JIFF
-    jiff_path = conf["jiff"]["jiff_path"]
-    party_count = conf["jiff"]["party_count"]
-    server_ip = conf["jiff"]["server_ip"]
-    server_port = conf["jiff"]["server_port"]
+    jiff_path = conf["backends"]["jiff"]["jiff_path"]
+    party_count = conf["backends"]["jiff"]["party_count"]
+    server_ip = conf["backends"]["jiff"]["server_ip"]
+    server_port = conf["backends"]["jiff"]["server_port"]
     jiff_config = JiffConfig(jiff_path, party_count, server_ip, server_port)
 
     # NET
@@ -49,14 +46,25 @@ def setup(conf: Dict):
     conclave_config = CodeGenConfig(workflow_name) \
         .with_spark_config(spark_config) \
         .with_oc_config(oc_config) \
-        .with_swift_config(swift_config) \
         .with_jiff_config(jiff_config) \
         .with_network_config(net_config)
+
+    if data_backend == "swift":
+        cfg = conf["swift"]
+        swift_config = SwiftConfig(cfg)
+        conclave_config.with_swift_config(swift_config)
+
+    elif data_backend == "dataverse":
+        # dv conf
+        pass
+
+    else:
+        print("No remote data backend source listed. Using local storage.\n")
 
     conclave_config.pid = pid
     conclave_config.all_pids = all_pids
     conclave_config.name = workflow_name
-    conclave_config.use_swift = use_swift
+    conclave_config.data_backend = data_backend
 
     conclave_config.code_path = conf["code_path"]
     conclave_config.output_path = conf["output_path"]
@@ -65,15 +73,15 @@ def setup(conf: Dict):
     return conclave_config
 
 
-def download_data(conclave_config):
+def download_swift_data(conclave_config):
     """
     Download data from Swift to local filesystem.
     """
 
     swift_cfg = conclave_config.system_configs['swift'].source
     data_dir = conclave_config.input_path
-    container = swift_cfg['DATA']['container_name']
-    files = swift_cfg['DATA']['files']
+    container = swift_cfg['data']['container_name']
+    files = swift_cfg['data']['files']
 
     swift_data = SwiftData(swift_cfg)
 
@@ -84,7 +92,7 @@ def download_data(conclave_config):
     swift_data.close_connection()
 
 
-def post_data(conclave_config):
+def post_swift_data(conclave_config):
     """
     Store locally held data on Swift.
 
@@ -92,11 +100,11 @@ def post_data(conclave_config):
 
     Should check to see if container exists in the future, and create it if it doesn't exist.
     """
-    input_swift_data = conclave_config.system_configs['swift'].source['DATA']['files']
+    input_swift_data = conclave_config.system_configs['swift'].source['data']['files']
 
     swift_cfg = conclave_config.system_configs['swift'].dest
     data_dir = conclave_config.input_path
-    container = swift_cfg['DATA']['container_name']
+    container = swift_cfg['data']['container_name']
 
     swift_data = SwiftData(swift_cfg)
 
@@ -117,22 +125,27 @@ def run(protocol: Callable, mpc_framework: str="jiff", local_framework: str="pyt
     Load parameters from config, download data from Swift,
     dispatch computation, and push results back to Swift.
     """
+
     parser = argparse.ArgumentParser(description="Run new workflow for Conclave.")
     parser.add_argument("--conf", metavar="/config/file.yml", type=str,
-                        help="path of the config file", default="conf-ca.yml", required=False)
+                        help="path of the config file", default="conf-ca.json", required=False)
 
     args = parser.parse_args()
 
     with open(args.conf) as fp:
-        conf = yaml.load(fp)
+        conf = json.load(fp)
 
     conclave_config = setup(conf)
 
-    if conclave_config.use_swift:
-        download_data(conclave_config)
+    if conclave_config.data_backend == "swift":
+        download_swift_data(conclave_config)
         generate_and_dispatch(protocol, conclave_config, [mpc_framework], [local_framework], apply_optimizations=False)
-        post_data(conclave_config)
+        post_swift_data(conclave_config)
 
+    elif conclave_config.data_backend == "dataverse":
+        # dv download // post protocols
+        pass
+    
     else:
 
         generate_and_dispatch(protocol, conclave_config, [mpc_framework], [local_framework], apply_optimizations=False)
