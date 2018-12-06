a534c7fac76e28372cfc167eb6fab4bba8c871fa
Ben <bengetch@gmail.com>
2018-12-06 14:55:00 -0500

    post data protocol

--- conclave/dataverse/__init__.py
+++ conclave/dataverse/__init__.py
@@ -26,35 +26,62 @@ class DataverseData:
         self.dataverse_connection = DataverseHandler().get_connection(cfg)
 
     def get_data(self, file_path):
+        """
+        Retrieve a file from Dataverse.
+        """
 
         if self.dataverse_connection is None:
             print("Previous connection was closed. Reinitialize this object.")
             return self
 
-        aliased_dv = self.dataverse_connection.get_dataverse(self.cfg["source"]['alias'])
-        dataset = aliased_dv.get_dataset_by_doi(self.cfg['source']['doi'])
-
-        files = dataset.get_files()
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         expected_files = set(self.cfg["source"]["files"])
         retrieved_files = set()
 
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        aliased_dv = self.dataverse_connection.get_dataverse(self.cfg["source"]['alias'])
+        dataset = aliased_dv.get_dataset_by_doi(self.cfg['source']['doi'])
+        files = dataset.get_files()
 
-        # TODO: might be a better way to do this
         for f in files:
             if f.name in expected_files:
                 retrieved_files.add(f.name)
                 download_url = f.download_url
-                # TODO: throw exception here if it fails
-                req = requests.get(download_url)
 
-                print("Writing object {0} to {1}.".format(f.name, file_path))
+                # TODO: handle download failure
+                req = requests.get(download_url, auth=self.dataverse_connection.auth)
 
                 with open("{0}/{1}".format(file_path, f.name), 'wb') as out:
                     out.write(req.content)
 
-        if expected_files != retrieved_files:
-            print("Not all files retrieved. Ensure that filenames passed "
-                  "correspond correctly to files stored on this Dataverse.")
+                print("Wrote object {0} to {1}.".format(f.name, file_path))
+
+        for f in expected_files:
+            if f not in retrieved_files:
+                print("Could not locate file {}. "
+                      "Check to make sure this file is stored on this Dataverse."
+                      .format(f))
+
+    def put_data(self, file_path, file):
+
+        if self.dataverse_connection is None:
+            print("Previous connection was closed. Reinitialize this object.")
+            return self
+
+        content = open("{0}/{1}".format(file_path, file), 'r').read()
+
+        aliased_dv = self.dataverse_connection.get_dataverse(self.cfg['dest']['alias'])
+        dataset = aliased_dv.create_dataset(
+            self.cfg['dest']['title'],
+            'Output data for {}.'.format(self.cfg['dest']['title']),
+            self.cfg['dest']['author']
+        )
+        dataset.upload_file(file, content)
+
+
+
+
+
+
+
 
 
