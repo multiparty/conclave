144224905be917c58ff82dd5d9691ba0108f9383
n1v0lg <n1v0lg@users.noreply.github.com>
2019-01-27 13:02:55 +0100

    Update run script

--- examples/benchmarks/comorb/run.sh
+++ examples/benchmarks/comorb/run.sh
@@ -2,13 +2,12 @@
 
 PARTY_ID=$1
 SIZE=$2
-DATA_ROOT_DIR=aspirin_data/conclave/${SIZE}/
+DATA_ROOT_DIR=comorb_data/conclave/${SIZE}/
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # clean up data first
 find /mnt/shared/${DATA_ROOT_DIR} \
-    -type f -not -name left_medication.csv -not -name left_diagnosis.csv \
-    -type f -not -name right_medication.csv -not -name right_diagnosis.csv \
+    -type f -not -name left_diagnosis.csv -not -name right_diagnosis.csv \
     -print0 | xargs -0 rm --;
 
 # run query
