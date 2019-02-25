47b7a31372b7173662d4049896aad151f43f5589
Ben <bengetch@gmail.com>
2019-02-25 14:05:02 -0500

    rm nose tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/multiparty/conclave',
+    url              = 'https://github.com/cici-conclave/conclave',
     description      = 'Infrastructure for defining and running large data workflows against multiple backends.',
     long_description = open('README.rst').read(),
     test_suite       = 'nose.collector',
